4c1e37bcc88b8736218c0702865828de2d8bd10e
Meterko <s_jcmeterko@clarion.edu>
2018-04-30 10:36:43 -0400

    Add 3 sounds and implement them into the game. (Healing, shooting, and going through doors)

--- ACFramework Version 2.04/ACFramework/game3D.cs
+++ ACFramework Version 2.04/ACFramework/game3D.cs
@@ -242,7 +242,7 @@ namespace ACFramework
 
         public override cCritterBullet shoot()
         {
-            Framework.snd.play(Sound.Pop);
+            Framework.snd.play(Sound.Gunfire);
             Sprite.ModelState = State.ShotButStillStanding;
             return base.shoot();
         }
@@ -623,7 +623,7 @@ namespace ACFramework
             if (distanceTo(pcritter) + pcritter.Radius < Radius + 1) //if player gets within radius+1 of pickup (close but not inside of it)
             {
                 //play sound for pickup, add score, add health, and make the treasure go away.
-                Framework.snd.play(Sound.Hallelujah);
+                Framework.snd.play(Sound.Healing);
                 pcritter.addScore(50);
                 pcritter.addHealth(5);
                 _collecteditem = true;  //set flag to determine when to spawn new item
@@ -1243,18 +1243,21 @@ namespace ACFramework
                 if (_currentroom == 0)
                 {
                     setRoom1();
+                    Framework.snd.play(Sound.Teleport);
                     doorcollision = false;
                 }
 
                 else if (_currentroom == 1)
                 {
                     setRoom2();
+                    Framework.snd.play(Sound.Teleport);
                     doorcollision = false;
                 }
 
                 else if (_currentroom == 2)
                 {
                     setRoom3();
+                    Framework.snd.play(Sound.Teleport);
                     doorcollision = false;
                 }
             }
